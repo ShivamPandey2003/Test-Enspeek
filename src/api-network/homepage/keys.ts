08ccce932b081478a0e8065fcbcb04b7b1dc2897
Vipin-88 <vipin.kumar@knowledgeexcel.com>
2026-05-15 21:52:36 +0530

    `Update homepage keys in keys.ts`

--- src/api-network/homepage/keys.ts
+++ src/api-network/homepage/keys.ts
@@ -3,11 +3,9 @@ import url from "../url";
 export const homepageKeys = {
   all: ["homepage"] as const,
 
-  studyList: (selection?: string) =>
-    [...homepageKeys.all, url.studyListing.queryKey, selection] as const,
+  studyList: (selection?: string) => [...homepageKeys.all, url.studyListing.queryKey, selection] as const,
 
-  userInfo: () =>
-    [...homepageKeys.all, url.userInfo.queryKey] as const,
+  userInfo: () => [...homepageKeys.all, url.userInfo.queryKey] as const,
 };
 
 export default homepageKeys;
